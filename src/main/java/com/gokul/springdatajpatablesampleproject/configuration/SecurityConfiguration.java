0836f8493b0024b89e0769f47f603a6f4233f879
gokul34-65 <gokulkarunaharan41@gmail.com>
2025-07-23 22:14:30 +0530

    disabled cors

--- src/main/java/com/gokul/springdatajpatablesampleproject/configuration/SecurityConfiguration.java
+++ src/main/java/com/gokul/springdatajpatablesampleproject/configuration/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.cors(Customizer.withDefaults());
+        http.cors(cors-> cors.disable());
         http.csrf(customizer -> customizer.disable());
         http.authorizeHttpRequests(customizer -> customizer
                 .requestMatchers("/auth/*").permitAll()
@@ -56,7 +56,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("https://thought-app-frontend.vercel.app","https://thought-app-frontend.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
