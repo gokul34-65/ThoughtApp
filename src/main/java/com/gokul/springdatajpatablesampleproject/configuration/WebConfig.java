3a57083c1a3f11d90c07f41391b70cb7ef41d8a4
gokul34-65 <gokulkarunaharan41@gmail.com>
2025-07-23 09:48:25 +0530

    production ready

---
+++ src/main/java/com/gokul/springdatajpatablesampleproject/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package com.gokul.springdatajpatablesampleproject.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Replace with your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
